1e4e505f9a1795db1d57f71663beed554c424fef
dengeist <eliasjmason@gmail.com>
2016-04-27 08:02:47 -0700

    Adds non-working delete method onto PStorage.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
 var Storage = function() {
     this.items = [];
@@ -15,16 +17,39 @@ Storage.prototype.add = function(name) {
     return item;
 };
 
+Storage.prototype.delete = function(name) {
+    var item = {
+        name: name,
+        id: this.id
+    };
+    var index = this.items[item.id];
+    //this.items.splice();
+};
+
 var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
+storage.delete('Peppers')
 
 var app = express();
 app.use(express.static('public'));
 
 app.get('/items', function(req, res) {
     res.json(storage.items);
+});
+
+app.post('/items', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    var item = storage.add(req.body.name);
+    res.status(201).json(item);
+});
+
+app.delete('/items/:id', jsonParser, function(req, res){
+    res.status(200).json(item);
 });
 
 app.listen(process.env.PORT || 8080);
